3e082f2233bd09a9d91874b6c71d9dd7577693db
Влад Чепурко <vlad113chepurko@gmail.com>
2025-10-29 15:40:26 +0200

    connected mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI);
+console.log("Connected to MongoDB");
+
 
 const PORT = process.env.PORT || 3000;
 
