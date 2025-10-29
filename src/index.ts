7062b9bc7e2fd66750b230689c26b6be1bcecc05
Влад Чепурко <vlad113chepurko@gmail.com>
2025-10-29 11:56:58 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
